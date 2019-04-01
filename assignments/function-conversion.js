3996bc65629f6bbfde3e4390d14bf49b9a4dcfde
Leonel <leonelaf12@gmail.com>
2019-04-01 17:13:29 -0400

    found out how to check answers. just need to console.log and then invoke.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,24 +1,24 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = () => {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+ let myFunction = () => {
+ console.log("Function was invoked!");
+ };
+ myFunction();
 
-// let anotherFunction = param => {
-//   return param;
-// };
-// anotherFunction(param);
+ let anotherFunction = param => {
+   return param;
+ };
+ console.log(anotherFunction(4));
 
-// let add = (param1, param2) => {
-//   return param1 + param2;
-// };
-// add(1,2);
+ let add = (param1, param2) => {
+   return param1 + param2;
+ };
+ console.log(add(1,2));
 
-// let subtract = (param1, param2) => {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+ let subtract = (param1, param2) => {
+   return param1 - param2;
+ };
+ console.log(subtract(1,2));
 
 
 // Stretch
